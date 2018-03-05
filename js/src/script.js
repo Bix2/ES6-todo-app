144a7035de1afa6063ba15477556e4dc21db3cf3
Bix2 <bibi.treffers@outlook.com>
2018-03-05 04:04:34 +0100

    saveToStorage function

--- js/src/script.js
+++ js/src/script.js
@@ -33,9 +33,9 @@ class Note {
   }
 
   saveToStorage(){
-    // HINT🤩
-    // localStorage only supports strings, not arrays
-    // if you want to store arrays, look at JSON.parse and JSON.stringify
+    // save to localstorage from App class
+    app.notesArray.push(this);
+    localStorage.setItem('notes', JSON.stringify(app.notesArray));
   }
 
   remove(){
